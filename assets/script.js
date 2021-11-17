912258a2d4768011bb7f7ae4b7e6726479888cc8
Steve Hairston <>
2021-11-16 23:11:43 -0500

    fixed counters

--- assets/script.js
+++ assets/script.js
@@ -14,52 +14,63 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function(){
-    // HINT: You can delete this console.log after you no longer need it!
-    
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-    document.getElementById('qty-gb').innerHTML = gb++
+        
+    //Code to be run when the "+" button for "Gingerbread" is clicked
+    gb++ ;
+    document.getElementById('qty-gb').innerHTML = gb ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
-// Event listener for clicks on the "+" button for Gingerbread cookies
+// Event listener for clicks on the "+" button for "Gingerbread" cookies
 document.getElementById('minus-gb').addEventListener('click', function() {
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-    document.getElementById('qty-gb').innerHTML = gb--
+    //Code to be run when the "-" button for "Gingerbread" is clicked
+    if(gb < 1){}
+    else{
+    gb-- ;
+    document.getElementById('qty-gb').innerHTML = gb ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
+    }
 })
 
-// Event listener for clicks on the "+" button for Chocolate Chip cookies
+// Event listener for clicks on the "+" button for "Chocolate Chip" cookies
 document.getElementById('add-cc').addEventListener('click', function(){
     
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-    document.getElementById('qty-cc').innerHTML = cc++
+    //Code to be run when the "+" button for "Chocolate Chip" is clicked
+    cc++ ;
+    document.getElementById('qty-cc').innerHTML = cc ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
-// Event listener for clicks on the "+" button for Chocolate Chip cookies
+// Event listener for clicks on the "+" button for "Chocolate Chip" cookies
 document.getElementById('minus-cc').addEventListener('click', function() {
   
-    // TODO: Write the code to be run when the "+" button for "Chocolate Chip" is clicked
-    document.getElementById('qty-cc').innerHTML = cc--
+    //Code to be run when the "-" button for "Chocolate Chip" is clicked
+    if(cc < 1){}
+    else{
+    cc-- ;
+    document.getElementById('qty-cc').innerHTML = cc ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
+    }
 })
 
 // Event listener for clicks on the "+" button for Sugar cookies
 document.getElementById('add-sugar').addEventListener('click', function(){
     
-    // TODO: Write the code to be run when the "+" button for "Sugar" is clicked
-    document.getElementById('qty-sugar').innerHTML = sugar++
+    //Code to be run when the "+" button for "Sugar" is clicked
+    sugar++ ;
+    document.getElementById('qty-sugar').innerHTML = sugar ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
 // Event listener for clicks on the "+" button for Sugar cookies
 document.getElementById('minus-sugar').addEventListener('click', function() {
 
-    // TODO: Write the code to be run when the "+" button for "Sugar" is clicked
-    document.getElementById('qty-sugar').innerHTML = sugar--
-})
-
-document.getElementById('qty-total').addEventListener('click', function() {
-
-    // TODO: Write the code to be run when the "+" button for "Sugar" is clicked
-    document.getElementById('qty-total').innerHTML = b + cc + sugar
+    //Code to be run when the "-" button for "Sugar" is clicked
+    if(sugar < 1){}
+    else{
+    sugar-- ;
+    document.getElementById('qty-sugar').innerHTML = sugar ;
+    document.getElementById('qty-total').innerHTML = gb + cc + sugar
+    }
 })
